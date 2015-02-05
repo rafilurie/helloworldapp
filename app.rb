05abc469eac08cfec2ad6fdc039e21266212003f
saasbook <saasbook@saasbook.(none)>
2015-02-05 00:24:52 -0400

    added more files

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+class MyApp < Sinatra::Base
+	get '/' do
+		"<!DOCTYPE html>
+			<html>
+			<head></head>
+				<body>
+					<h1>Hello, World!</h1>
+				</body>
+			</html>
+		"
+	end
+end
